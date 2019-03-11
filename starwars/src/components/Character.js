3be88673ba12909d81124de223dc4824577e9461
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-03-11 10:23:09 +0800

    Add character information to Character component

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -2,10 +2,14 @@ import React from 'react'
 import './StarWars.css'
 
 const Character = props => {
-  const { name } = props
+  const { name, height, mass, gender, birth_year } = props.character
   return (
-    <div className="character">
-
+    <div className="character-info">
+      <h3>{name}</h3>
+      <p><strong>Height:</strong> {height}</p>
+      <p><strong>Mass:</strong> {mass}</p>
+      <p><strong>Gender:</strong> {gender}</p>
+      <p><strong>Birth Year:</strong> {birth_year}</p>
     </div>
   )
 }

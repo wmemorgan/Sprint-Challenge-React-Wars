60ff2603fa671a8b046fee3ff897df6a2f025723
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-03-11 18:31:55 +0800

    Add eye and hair color information

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -2,14 +2,16 @@ import React from 'react'
 import './StarWars.css'
 
 const Character = props => {
-  const { name, height, mass, gender, birth_year } = props.character
+  const { name, gender, birth_year, height, mass, eye_color, hair_color } = props.character
   return (
     <div className="character-info">
       <h3 className="header-character">{name}</h3>
-      <p><span className="stat-category">Height:</span> {height}</p>
-      <p><span className="stat-category">Mass:</span> {mass}</p>
       <p><span className="stat-category">Gender:</span> {gender}</p>
       <p><span className="stat-category">Birth Year:</span> {birth_year}</p>
+      <p><span className="stat-category">Height:</span> {height}</p>
+      <p><span className="stat-category">Mass:</span> {mass}</p>
+      <p><span className="stat-category">Eye Color:</span> {eye_color}</p>
+      <p><span className="stat-category">Hair Color:</span> {hair_color}</p>
     </div>
   )
 }

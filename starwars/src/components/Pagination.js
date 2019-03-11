7017f4606d10a4523e3bd2a1ad6792630a5b2892
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-03-11 19:02:59 +0800

    Refactor previous and next page buttons

--- starwars/src/components/Pagination.js
+++ starwars/src/components/Pagination.js
@@ -2,14 +2,15 @@ import React from 'react'
 import classNames from 'classnames'
 
 const Pagination = props => {
-  const { totalRecords, pageLimit, partialURL, currentPage,
-    nextPage, getCharacters } = props
+  const { totalRecords, pageLimit, partialURL, currentPage, getCharacters } = props
   const goToPage = number => partialURL + number 
-  const previousPage = props.previousPage ? props.previousPage : 'https://swapi.co/api/people/?page=1'
   const pageNumbers = [];
   for (let i = 1; i <= Math.ceil(totalRecords / pageLimit); i++) {
     pageNumbers.push(i);
   }
+
+  const incrementPage = currentPage + 1 >= pageNumbers.length ? pageNumbers.length : currentPage + 1
+  const decrementPage = currentPage - 1 <= 1 ? 1 : currentPage - 1
 
   return (
     <div className="pagination-container">
@@ -21,27 +22,30 @@ const Pagination = props => {
       </div>
       <div 
         className="page-number"
-        onClick={() => getCharacters(previousPage, currentPage - 1)}
+        onClick={() => getCharacters(goToPage(decrementPage), decrementPage)}
       >
         Previous
       </div>
+
       {pageNumbers.map(number => {
         let pageNumberClassGroup = classNames({
           "page-number": true,
           active: currentPage === number
         });
-      return (
-        <div 
-          className={pageNumberClassGroup} 
-          key={number} 
-          onClick={() => getCharacters(goToPage(number), number)}
-        >
-          {number}
-        </div>
-      )})}
+        return (
+          <div 
+            className={pageNumberClassGroup} 
+            key={number} 
+            onClick={() => getCharacters(goToPage(number), number)}
+          >
+            {number}
+          </div>
+        )
+      })}
+
       <div 
         className="page-number"
-        onClick={() => getCharacters(nextPage)}
+        onClick={() => getCharacters(goToPage(incrementPage), incrementPage)}
       >
         Next
       </div>
